cad5a485953b332bc871dc58e11af6a101d26e79
Jakub Kozłowski <kubukoz@gmail.com>
2023-01-15 00:33:23 +0100

    change single derivation

--- example/src/Main.scala
+++ example/src/Main.scala
@@ -6,7 +6,7 @@ import cats.effect._
 
 object Main extends IOApp.Simple {
   def run = {
-    IO.println(colorize"hello there".red.render) *>
+    IO.println(colorize"hello there, now updated!".red.render) *>
       IO.println(1 |+| 10) *>
       IO.println(hello.Hello(s = "aa"))
   }
